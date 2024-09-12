852c382cdbb531a4aa36e53c68252a3392fed82e
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-09-12 12:51:23 +0500

    UI: country card, Feat: Made the card dynamic

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,1 +1,39 @@
-// A single item to display a country
+interface countryProps {
+    imageUrl: string;
+    name: string;
+    population: number;
+    region: string;
+    capital: string;
+}
+
+export default function Country({
+    imageUrl,
+    name,
+    population,
+    region,
+    capital,
+}: countryProps) {
+    return (
+        <div className=" flex flex-col overflow-hidden dark:bg-[#2b3945] transition-colors duration-300 rounded-xl shadow-md">
+            <img src={imageUrl} alt="" className="w-full h-[200px]" />
+            <div className=" dark:text-white p-6">
+                <h1 className="font-bold mb-4">{name}</h1>
+                <p className=" mb-2 ">
+                    {" "}
+                    <span className=" font-semibold">Population</span>{" "}
+                    {population}
+                </p>
+
+                <p className=" mb-2 ">
+                    {" "}
+                    <span className=" font-semibold">Region</span> {region}
+                </p>
+
+                <p className=" mb-2 ">
+                    {" "}
+                    <span className=" font-semibold">Capital</span> {capital}
+                </p>
+            </div>
+        </div>
+    );
+}

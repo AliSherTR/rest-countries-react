41ee1979825bfb70ef6709b35b6e4fc81f08fc46
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-09-12 20:44:41 +0500

    UI: UI adjustments

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 interface countryProps {
     imageUrl: string;
     name: string;
@@ -14,26 +16,29 @@ export default function Country({
     capital,
 }: countryProps) {
     return (
-        <div className=" flex flex-col overflow-hidden dark:bg-[#2b3945] transition-colors duration-300 rounded-xl shadow-md">
-            <img src={imageUrl} alt="" className="w-full h-[200px]" />
-            <div className=" dark:text-white p-6">
-                <h1 className="font-bold mb-4">{name}</h1>
-                <p className=" mb-2 ">
-                    {" "}
-                    <span className=" font-semibold">Population</span>{" "}
-                    {population}
-                </p>
+        <Link to="/country">
+            <div className=" flex flex-col overflow-hidden dark:bg-[#2b3945] transition-colors duration-300 rounded-xl shadow-md">
+                <img src={imageUrl} alt="" className="w-full h-[200px]" />
+                <div className=" dark:text-white p-6">
+                    <h1 className="font-bold mb-4">{name}</h1>
+                    <p className=" mb-2 ">
+                        {" "}
+                        <span className=" font-semibold">Population</span>{" "}
+                        {population}
+                    </p>
 
-                <p className=" mb-2 ">
-                    {" "}
-                    <span className=" font-semibold">Region</span> {region}
-                </p>
+                    <p className=" mb-2 ">
+                        {" "}
+                        <span className=" font-semibold">Region</span> {region}
+                    </p>
 
-                <p className=" mb-2 ">
-                    {" "}
-                    <span className=" font-semibold">Capital</span> {capital}
-                </p>
+                    <p className=" mb-2 ">
+                        {" "}
+                        <span className=" font-semibold">Capital</span>{" "}
+                        {capital}
+                    </p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }

2d9506cc908c8267ac06ff8cd783dbc35b26d4fb
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-09-12 20:10:00 +0500

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "./components/theme-provider";
 import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
 
 export default function App() {
     return (
-        <>
-            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <HomePage />
-            </ThemeProvider>
-        </>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/country" element={<Header />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }

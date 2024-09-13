1175a1493885842cdec7332eb4eff992351e1775
AliSherTR <94902748+AliSherTR@users.noreply.github.com>
2024-09-13 14:59:49 +0500

    UI: loading state component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className=" dark:bg-[#202c37] transition-colors duration-300 min-h-screen flex items-center justify-center">
+            <h2 className=" text-lg dark:text-white">Searching.....</h2>
+        </div>
+    );
+}
